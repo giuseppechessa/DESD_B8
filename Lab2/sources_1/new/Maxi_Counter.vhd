866063c98247c5d55a1f139e2771c69ab76c490e
TeoVallone <teo.vallone@mail.polimi.it>
2022-04-03 16:30:00 +0200

    Update Maxi_Counter.vhd
    Aggiunti commenti e spazi, ho rinominato number_of_pulses in num_of_pulses così i nomi escono coerenti, quindi va modificata la testbench se riuscite (dovrebbe comparire solo un paio di volte se non sbaglio)

--- Lab2/sources_1/new/Maxi_Counter.vhd
+++ Lab2/sources_1/new/Maxi_Counter.vhd
@@ -1,55 +1,78 @@
 library IEEE;
 	use IEEE.STD_LOGIC_1164.ALL;
 	use IEEE.NUMERIC_STD.ALL;
-	use IEEE.math_real.ALL;
+	use IEEE.MATH_REAL.ALL;
 	
 entity Maxi_Counter is
   Generic(
-		Number_Of_Pulses	:	POSITIVE	:= 1e6;
+		NUM_OF_PULSES	    :	POSITIVE	:= 1e6;	-- Number of clock cycles between two register shifts at base speed
 		NUM_OF_SWS			:	INTEGER		RANGE	1 TO 16 := 16	-- Number of input switches
   );
   Port (
-		clk		:	in	std_logic;
-		reset	:	in	std_logic;
+		clk			:	in	std_logic;
+		reset		:	in	std_logic;
 		
-		Switches	:	in	std_logic_vector(NUM_OF_SWS-1 DOWNTO 0);
-		enable	:	out	std_logic
+		switches	:	in	std_logic_vector(NUM_OF_SWS-1 DOWNTO 0);
+		enable		:	out	std_logic
   );
 end Maxi_Counter;
 
 architecture Behavioral of Maxi_Counter is
-    constant Zero_Value     :unsigned(NUM_OF_SWS-1 DOWNTO 0) := (Others=>'0');
 
-	signal PrimaryCounter	:	unsigned(integer(floor(log2(real(Number_Of_Pulses)))) DOWNTO 0):= (Others=>'0');
-	signal SecondaryCounter	:	unsigned(NUM_OF_SWS-1 DOWNTO 0) := (Others=>'0');
-	signal Switches_value	:	unsigned(NUM_OF_SWS-1 DOWNTO 0) := (Others=>'0');
-	signal enable_proxy    	:	std_logic := '0';
+    constant Zero_Value         :   unsigned(NUM_OF_SWS-1 DOWNTO 0) := (Others => '0');
+
+	-- Base Counter used to count the number of clock cycles between register shifts at base speed
+	signal Primary_Counter	:	unsigned(integer(floor(log2(real(NUM_OF_PULSES)))) DOWNTO 0):= (Others => '0');
+	-- When any switch is on, this additional Counter counts the number of times PrimaryCounter has counted to NUM_OF_PULSES
+	signal Secondary_Counter	:	unsigned(NUM_OF_SWS-1 DOWNTO 0) := (Others => '0');
+
+	-- Equivalent numerical value determined by the Switches
+	signal Switches_Value	    :	unsigned(NUM_OF_SWS-1 DOWNTO 0) := (Others => '0');
+	
+	signal Enable_Reg    	    :	std_logic := '0';
 	
 begin
-    enable<=enable_proxy;
-	process (clk,reset)
-	begin
-		if reset= '1' then
-			enable_proxy<='0';
-			PrimaryCounter<= (Others=>'0');
-			SecondaryCounter<= (Others=>'0');
-			Switches_value<=unsigned(Switches);
-		elsif rising_edge(clk) then
-			enable_proxy<='0';
-			PrimaryCounter<=PrimaryCounter+1;
-			if PrimaryCounter>=to_unsigned(Number_Of_Pulses,PrimaryCounter'LENGTH) then
-				PrimaryCounter<= (Others=>'0');
-				SecondaryCounter<=SecondaryCounter+1;
-				if SecondaryCounter>=unsigned(Switches_value) then
-					SecondaryCounter<= (Others=>'0');
-					enable_proxy<='1';
-				end if;
-			end if;
-			if SecondaryCounter=Zero_Value then
-				Switches_value<=unsigned(Switches);
-			end if;
-		end if;
-	end process;
 
+    enable <= Enable_Reg;
+    
+	process (clk, reset)
+	begin
+	    -- Whenever we press reset, we reset both counters and check if the switches have changed
+		if reset = '1' then
+		
+			Enable_Reg <= '0';
+			
+			Primary_Counter <= (Others => '0');
+			Secondary_Counter <= (Others => '0');
+			
+			Switches_Value <= unsigned(switches);
+			
+		elsif rising_edge(clk) then
+		
+			Enable_Reg <= '0';
+			Primary_Counter <= Primary_Counter + 1;
+			
+			-- When the first counter completes a full loop we reset it to 0 and increase the second counter by 1
+			if Primary_Counter >= to_unsigned(NUM_OF_PULSES-1, Primary_Counter'LENGTH) then
+			
+				Primary_Counter <= (Others => '0');
+				Secondary_Counter <= Secondary_Counter + 1;
+				
+				-- We can only activate the enable if the second counter has reached the value represented by the switches
+				if Secondary_Counter >= unsigned(Switches_Value) then
+					Secondary_Counter <= (Others => '0');
+					Enable_Reg<='1';
+				end if;
+				
+			end if;
+			
+			-- We check to see if the switches have been changed only at the end of each complete loop,  i.e. ehen enable has been activated, and at the beginning
+			if Secondary_Counter = Zero_Value then
+				Switches_Value <= unsigned(switches);
+			end if;
+			
+		end if;
+		
+	end process;
 
 end Behavioral;
