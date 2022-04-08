4eb16a4b38548bd34d08d5a3b8a2fc3df14597e0
TeoVallone <102909361+TeoVallone@users.noreply.github.com>
2022-04-08 13:59:32 +0200

    ho allineato un po di righe che non erano allineate #SpaziettiForGarzetti

--- Lab2/sources_1/new/Mini_Counter.vhd
+++ Lab2/sources_1/new/Mini_Counter.vhd
@@ -1,11 +1,11 @@
 library IEEE;
 	use IEEE.STD_LOGIC_1164.ALL;
 	use IEEE.NUMERIC_STD.ALL;
-	use IEEE.math_real.ALL;
+	use IEEE.MATH_REAL.ALL;
 
 entity Mini_Counter is
 	Generic(
-		TAIL_LENGTH				:	INTEGER	RANGE	1 TO 16	:= 4	-- Tail length
+		TAIL_LENGTH		:	INTEGER	RANGE	1 TO 16	:= 4  -- Tail length
 	);
     Port ( 
 		clk		: 	in 	std_logic;
@@ -13,31 +13,43 @@ entity Mini_Counter is
 		
 		din		:	in	std_logic;
 		enable	:	in	std_logic;
-		dout	:	out	std_logic_vector(integer(log2(real(TAIL_LENGTH))) DOWNTO 0) --It has to be long only the necessary bits
+
+		dout	:	out	std_logic_vector(integer(log2(real(TAIL_LENGTH))) DOWNTO 0) --We design dout to have as many bits as we need and no more than that
 	);
 end Mini_Counter;
 
 architecture Behavioral of Mini_Counter is
-	signal	Counter	: unsigned(integer(log2(real(TAIL_LENGTH))) DOWNTO 0):=(Others=>'0'); --Internal Counter
-	signal  Sub		: std_logic;
+
+	signal	Counter			: unsigned(integer(log2(real(TAIL_LENGTH))) DOWNTO 0) := (Others => '0'); --Internal Counter
+	signal  Enable_Sig		: std_logic;
+
 begin
-	dout<=std_logic_vector(Counter);
+
+	dout <= std_logic_vector(Counter);
 	
-	process (clk,reset)
+	process (clk, reset)
 	begin
-		if reset='1' then
-			Counter<=(Others=>'0');  --asincronous reset
+
+		if reset = '1' then
+
+			Counter <= (Others => '0');  --Asyncronous reset
+
 		elsif rising_edge(clk) then
-			if din='1' then
-				Counter<=to_unsigned(TAIL_LENGTH,Counter'LENGTH); --it goes up to TAIL_LENGTH when it has an input
-			elsif din='0' and Counter>0 and Sub='0' then
-				Counter<=Counter-1;  -- it decreses until 0 when no input is applied
-				Sub<='1';
+
+			if din = '1' then
+				Counter <= to_unsigned(TAIL_LENGTH, Counter'LENGTH); --Counter is set to TAIL_LENGTH when we have din = '1' as input
+
+			elsif din = '0' and Counter > 0 and Enable_Sig = '1' then
+				Counter <= Counter - 1;  --Counter is decreased until 0 when no input is applied, one clock cycle after enable is 1: Enable_Sig is necessary to obtain a tail with the right length
+				Enable_Sig <= '0';
 			end if;
-			if enable='1' then  --only in case of enable that is high, so we can simulate a slower clk
-                Sub <='0';
+
+			if enable = '1' then  --This condition makes it so the counter can only be decreased in the clock cycle after enable is set to 1
+                Enable_Sig <='1';
 			end if;
+
 		end if;
+
 	end process;
 
 end Behavioral;
--- Lab2/sources_1/new/Shift_Register.vhd
+++ Lab2/sources_1/new/Shift_Register.vhd
@@ -3,60 +3,60 @@ library IEEE;
 
 entity Shift_Register is
 	Generic(
-		NUM_OF_LEDS		:	INTEGER	RANGE	1 TO 16 := 16	-- Number of output LEDs
+		NUM_OF_LEDS		:	INTEGER	RANGE 1 TO 16 := 16	-- Number of output LEDs
 	);
     Port ( 
-		clk		:in 	std_logic;
-        reset	:in 	std_logic;
+		clk		: in 	std_logic;
+        reset	: in 	std_logic;
 		
-		enable	:in		std_logic;	-- Uses the output of Maxi_Counter to determine when we can shift the registers
+		enable	: in	std_logic;	-- Uses the output of Maxi_Counter to determine when we can shift the registers
 
-		Led_Out	:out	std_logic_vector(NUM_OF_LEDS-1 DOWNTO 0)
+		Led_Out	: out	std_logic_vector(NUM_OF_LEDS-1 DOWNTO 0)
 	);
 end Shift_Register;
 
 architecture Behavioral of Shift_Register is
 
-	signal SR_internal	:	std_logic_vector(NUM_OF_LEDS-1 DOWNTO 0) := (0 => '1', Others => '0');
+	signal SR_internal	: std_logic_vector(NUM_OF_LEDS-1 DOWNTO 0) := (0 => '1', Others => '0');
 
 	type Type_Dir is (GOING_RIGHT, GOING_LEFT);
-	signal Direction    :   Type_Dir := GOING_LEFT;
+	signal Direction    : Type_Dir := GOING_LEFT;
 
 begin
 
-	NUM_LEDS_ONE :      if NUM_OF_LEDS = 1 generate
+	NUM_LEDS_ONE 		:	if NUM_OF_LEDS = 1 generate
 		Led_Out <= (Others => '1');
 	end generate;
 	
-	NUM_LEDS_MULTIPLE : if NUM_OF_LEDS > 1 generate
+	NUM_LEDS_MULTIPLE 	: 	if NUM_OF_LEDS > 1 generate
 	
 		Led_out <= SR_Internal;
 		
-		process (clk,reset)
+		process (clk, reset)
 		begin
 
 			if reset = '1' then
 			    -- With a tail made by N LEDs we'll instantiate N SR, the one with the most intense PWM as input is initially placed on the MSB
 				SR_Internal <= (0 => '1', Others => '0');
 
-				direction <= GOING_LEFT;
+				Direction <= GOING_LEFT;
 
 			elsif rising_edge(clk) then
 				-- We'll apply a shift only whenever Counter enables it, in our case only when dtx has passed
 				if enable = '1' then 
 				    
 				    -- The Vector is shifted depending on the direction
-					if direction = GOING_LEFT then  
+					if Direction = GOING_LEFT then  
 						SR_Internal <= SR_Internal(NUM_OF_LEDS - 2 DOWNTO 0) & '0';
-					elsif direction = GOING_RIGHT then
+					elsif Direction = GOING_RIGHT then
 						SR_Internal <= '0' & SR_Internal(NUM_OF_LEDS - 1 DOWNTO 1);
 					end if;
 					
 					-- We switch direction whenever the single active LED of this instance approaches one of the two ends
-					if SR_Internal(SR_internal'HIGH - 1) = '1' AND direction = GOING_LEFT  then
-						direction <= GOING_RIGHT;
-					elsif SR_Internal(SR_internal'LOW + 1) = '1' AND direction = GOING_RIGHT then
-					    direction <= GOING_LEFT;
+					if SR_Internal(SR_internal'HIGH - 1) = '1' AND Direction = GOING_LEFT  then
+						Direction <= GOING_RIGHT;
+					elsif SR_Internal(SR_internal'LOW + 1) = '1' AND Direction = GOING_RIGHT then
+					    Direction <= GOING_LEFT;
 					end if;
 					
 				end if;
