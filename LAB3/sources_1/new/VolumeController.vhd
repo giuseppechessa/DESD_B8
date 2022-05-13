2763f8d0249abde76da4c07fd98d84c8f17ea30a
GppChss <giuseppe.chessa@mail.polimi.it>
2022-05-13 15:03:52 +0200

    preparate cartelle labbo3

---
+++ LAB3/sources_1/new/VolumeController.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity VolumeController.vhd is
+	generic (
+		NBITSNORMAL : INTEGER := 6
+	);
+	Port ( 
+		aclk 			: in  STD_LOGIC;
+		aresetn			: in  STD_LOGIC;
+
+	);
+end VolumeController.vhd;
+
+architecture Behavioral of LED_Unpacker is
+
+
+	
+begin
+    
+     
+
+end Behavioral;
