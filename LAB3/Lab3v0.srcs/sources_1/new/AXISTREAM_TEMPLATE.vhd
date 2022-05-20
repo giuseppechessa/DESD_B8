55c99a6cc6c87da774fee124e82511a23cb3f730
GppChss <giuseppe.chessa@mail.polimi.it>
2022-05-20 17:00:08 +0200

    eliminato i modulini comodi per un FANTASTICO template NON TESTATO che testare è roba da deboli

---
+++ LAB3/Lab3v0.srcs/sources_1/new/AXISTREAM_TEMPLATE.vhd
@@ -0,0 +1,128 @@
+library IEEE;
+	use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity AXISTREAM_TEMPLATE is
+	Generic(
+		boh_cose_varie	: Integer;
+	);
+  Port ( 
+	aclk			:	in	std_logic;
+	aresetn			:	in	std_logic;
+	
+	s_axis_tdata    :	in	std_logic_vector(24-1 DOWNTO 0);
+    s_axis_tvalid   :	in	std_logic;
+    s_axis_tlast    :	in	std_logic;
+    s_axis_tready   :	out	std_logic;
+	
+	m_axis_tdata    :	out	std_logic_vector(24-1 DOWNTO 0);
+    m_axis_tvalid   :	out	std_logic;
+    m_axis_tlast    :	out	std_logic;
+    m_axis_tready   :	in	std_logic
+  
+  
+  );
+end AXISTREAM_TEMPLATE;
+
+architecture Behavioral of AXISTREAM_TEMPLATE is
+	constant Pipeline_legth : signed(4 downto 0) :=0x"4";
+	
+	type	RxAxis_t	is enum(IDLE,Rx_READY,Elaboration_sx,Elaboration_dx);
+	signal	RxAxis	:	RxAxis_t:=IDLE;
+	
+	type	TxAxis_t	is enum(IDLE,Tx);
+	signal	TxAxis	:	RxAxis_t:=IDLE;
+	
+	signal	s_axis_tready_AUX	:	std_logic;
+	signal	m_axis_tvalid_AUX	:	std_logic;
+    signal	m_axis_tlast_AUX	:	std_logic;
+	
+	signal	Audio_sx_in	:	signed(24-1 DOWNTO 0);
+	signal	Audio_dx_in	:	signed(24-1 DOWNTO 0);
+	
+	signal	Audio_sx_out	:	signed(24-1 DOWNTO 0);
+	signal	Audio_dx_out	:	signed(24-1 DOWNTO 0);
+	
+	signal Counter_Pipeline	:	signed(4 downto 0):= Pipeline_legth;
+	signal Combinatorial_Finished	:	std_logic;
+begin
+
+	m_axis_tlast<=m_axis_tlast_AUX;
+	m_axis_tvalid<=m_axis_tvalid_AUX;
+	s_axis_tready<=s_axis_tready_AUX;
+
+	RxProcess : process (aclk)
+	begin
+	if rising_edge(clk)
+		if aresetn='0' then
+			RxAxis<=IDLE;
+		elsif 
+			case RxAxis is
+				when IDLE=>
+					signal Counter_Pipeline	:	signed(4 downto 0):= Pipeline_legth;
+					Combinatorial_Finished<='0';
+					s_axis_tready_AUX<='1';
+					RxAxis<=Rx_READY;
+				when Rx_READY=>
+					if s_axis_tvalid='1' and s_axis_tready_AUX='1' then
+						if s_axis_tlast='1' then
+							RxAxis<=Elaboration_dx;
+							Audio_sx_in<=signed(s_axis_tdata);
+						elsif 
+							RxAxis<=Elaboration_sx;
+							Audio_dx_in<=signed(s_axis_tdata);
+						end if;
+						s_axis_tready_AUX<='0';
+					end if;
+				when Elaboration_sx=>
+					Counter_Pipeline<=Counter_Pipeline-1;
+					if Counter_Pipeline=(Others=>'0') then 
+						RxAxis<=IDLE;
+						Combinatorial_Finished<='1';
+						m_axis_tlast_AUX<='0';
+					end if;
+				when Elaboration_dx=>
+					Counter_Pipeline<=Counter_Pipeline-1;
+					if Counter_Pipeline=(Others=>'0') then 
+						RxAxis<=IDLE;
+						Combinatorial_Finished<='1';
+						m_axis_tlast_AUX<='1';
+					end if;
+				when Others=>
+					RxAxis<=IDLE;
+			end case;
+		end if;
+	end if;
+	end process;
+	
+	TxProcess : process (aclk)
+	begin
+	if rising_edge(clk)
+		if aresetn='0' then
+			TxAxis<=IDLE;
+		elsif 
+			case TxAxis is
+				when IDLE=>
+					if Combinatorial_Finished='1' then 
+						if m_axis_tlast_AUX='1' then 
+							m_axis_tdata<=std_logic_vector(Audio_dx_out);
+						elsif m_axis_tlast_AUX='0' then
+							m_axis_tdata<=std_logic_vector(Audio_sx_out);
+						end if;
+						TxAxis<=Tx;
+						m_axis_tvalid_AUX<='1';
+					end if;
+				when Tx=>
+					if m_axis_tvalid_AUX='1' and m_axis_tready='1' then
+						m_axis_tvalid_AUX<='0';
+						TxAxis<=IDLE;
+					end if;
+				when Others=>
+					TxAxis<=IDLE;
+			end case;
+		end if;
+	end if;
+	end process;
+
+
+end Behavioral;
