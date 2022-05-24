a9d2793f1754d260f7fa78b9141a4655557a17d3
gabrieleroselli <102916851+gabrieleroselli@users.noreply.github.com>
2022-05-24 22:24:32 +0200

    comments

--- LAB3/sources_1/new/Filez belli funzionelli/dual_moving_average.vhd
+++ LAB3/sources_1/new/Filez belli funzionelli/dual_moving_average.vhd
@@ -3,14 +3,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity dual_moving_average is
     Generic(
-		DATA_LENGTH	:	Integer:= 24;
-        n_samples : integer := 32
+		DATA_LENGTH		: Integer := 24;
+		CLK_FREQUENCY 	: Integer := 100_000_000;
+        n_samples 		: integer := 32
     );
     Port (
-        aclk     : in std_logic;
-        aresetn     : in std_logic;
+        aclk     		: in std_logic;
+        aresetn     	: in std_logic;
         
-        filter_enable : in std_logic;
+        filter_enable	: in std_logic;
         
         s_axis_tdata    : in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
         s_axis_tvalid   : in std_logic;
@@ -26,37 +27,39 @@ end dual_moving_average;
 
 architecture Behavioral of dual_moving_average is
 
+	-- COMPONENTS -- 
     component data_receiver is
 		Generic(
 			DATA_LENGTH	:	Integer:= 24
 		);
         Port (
-            aclk     : in std_logic;
-            aresetn     : in std_logic;
+            aclk     		: in std_logic;
+            aresetn     	: in std_logic;
             
             s_axis_tdata    : in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
             s_axis_tvalid   : in std_logic;
             s_axis_tlast    : in std_logic;
             s_axis_tready   : out std_logic;
             
-            data_left : out std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
-            data_right : out std_logic_vector(DATA_LENGTH-1 DOWNTO 0)
+            data_left 		: out std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+            data_right 		: out std_logic_vector(DATA_LENGTH-1 DOWNTO 0)
         );
     end component;
     
     component mono_moving_average is
         Generic(
-			DATA_LENGTH	:	Integer:= 24;
-            n_samples : integer := 32
+			DATA_LENGTH		: Integer := 24;
+			CLK_FREQUENCY 	: Integer := 100_000_000;
+            n_samples 		: integer := 32
         );
         Port (
-            aclk     : in std_logic;
-            aresetn     : in std_logic;
+            aclk     		: in std_logic;
+            aresetn     	: in std_logic;
             
-            filter_enable : in std_logic;
+            filter_enable 	: in std_logic;
             
-            din : in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
-            dout : out std_logic_vector(DATA_LENGTH-1 DOWNTO 0)
+            din 			: in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+            dout 			: out std_logic_vector(DATA_LENGTH-1 DOWNTO 0)
         );
     end component;
     
@@ -65,11 +68,11 @@ architecture Behavioral of dual_moving_average is
 			DATA_LENGTH	:	Integer:= 24
 		);
         Port (
-            aclk     : in std_logic;
-            aresetn     : in std_logic;
+            aclk     		: in std_logic;
+            aresetn     	: in std_logic;
             
-            data_right    : in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
-            data_left    : in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+            data_right    	: in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+            data_left    	: in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
             
             m_axis_tdata    : out std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
             m_axis_tvalid   : out std_logic;
@@ -78,68 +81,77 @@ architecture Behavioral of dual_moving_average is
         );
     end component;
     
-    signal s_left_data : std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+	-- SIGNALS --
+	-- internal signal for the right and left channels
+    signal s_left_data 	: std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
     signal s_right_data : std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
     
-    signal m_left_data : std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+    signal m_left_data 	: std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
     signal m_right_data : std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
     
 begin
-
+	-- INSTANCES --
+	-- axi4-stream interface : slave part . The data received by the slave interface are sent to the moving average modules
     S_AXIS_PORT_INST : data_receiver
 	Generic Map(
-		DATA_LENGTH=>DATA_LENGTH
+		DATA_LENGTH 	=> DATA_LENGTH
 	)
     Port Map(
-        aclk => aclk,
-        aresetn => aresetn,
-        s_axis_tdata => s_axis_tdata,
-        s_axis_tvalid => s_axis_tvalid,
-        s_axis_tlast => s_axis_tlast, 
-        s_axis_tready => s_axis_tready,
-        data_left => s_left_data,
-        data_right => s_right_data
+        aclk 		  	=> aclk,
+        aresetn		  	=> aresetn,
+        s_axis_tdata  	=> s_axis_tdata,
+        s_axis_tvalid 	=> s_axis_tvalid,
+        s_axis_tlast  	=> s_axis_tlast, 
+        s_axis_tready 	=> s_axis_tready,
+        data_left 		=> s_left_data,
+        data_right		=> s_right_data
     );
     
+	-- We use two different instances to separately calculate the filtering effect in the right and left channels.
+	-- moving average module for the left channel 
     LEFT_AVERAGE : mono_moving_average
     Generic Map(
-		DATA_LENGTH=>DATA_LENGTH,
-        n_samples => n_samples
+		DATA_LENGTH		=> DATA_LENGTH,
+		CLK_FREQUENCY   => CLK_FREQUENCY,
+        n_samples   	=> n_samples
     )
     Port Map(
-        aclk => aclk,
-        aresetn => aresetn,
-        filter_enable => filter_enable,
-        din => s_left_data,
-        dout => m_left_data
+        aclk 			=> aclk,
+        aresetn 		=> aresetn,
+        filter_enable 	=> filter_enable,
+        din 			=> s_left_data,
+        dout 			=> m_left_data
     );
     
+	-- moving average module for the right channel 
     RIGHT_AVERAGE : mono_moving_average
     Generic Map(
-		DATA_LENGTH=>DATA_LENGTH,
-        n_samples => n_samples
+		DATA_LENGTH		=> DATA_LENGTH,
+		CLK_FREQUENCY   => CLK_FREQUENCY,
+        n_samples 		=> n_samples
     )
     Port Map(
-        aclk => aclk,
-        aresetn => aresetn,
-        filter_enable => filter_enable,
-        din => s_right_data,
-        dout => m_right_data
+        aclk 			=> aclk,
+        aresetn 		=> aresetn,
+        filter_enable 	=> filter_enable,
+        din 			=> s_right_data,
+        dout 			=> m_right_data
     );
-        
+    
+    -- axi4-stream interface : master part . The data coming from the moving average modules are sent through the master interface 
     M_AXIS_PORT_INST : data_sender
 	Generic Map(
-		DATA_LENGTH=>DATA_LENGTH
+		DATA_LENGTH 	=> DATA_LENGTH
 	)
     Port Map(
-        aclk => aclk,
-        aresetn => aresetn,
-        m_axis_tdata => m_axis_tdata,
-        m_axis_tvalid => m_axis_tvalid,
-        m_axis_tlast => m_axis_tlast, 
-        m_axis_tready => m_axis_tready,
-        data_left => m_left_data,
-        data_right => m_right_data
+        aclk 			=> aclk,
+        aresetn 		=> aresetn,
+        m_axis_tdata 	=> m_axis_tdata,
+        m_axis_tvalid 	=> m_axis_tvalid,
+        m_axis_tlast	=> m_axis_tlast, 
+        m_axis_tready 	=> m_axis_tready,
+        data_left 		=> m_left_data,
+        data_right 		=> m_right_data
     );
     
 end Behavioral;
--- LAB3/sources_1/new/Filez belli funzionelli/mono_moving_average.vhd
+++ LAB3/sources_1/new/Filez belli funzionelli/mono_moving_average.vhd
@@ -5,35 +5,47 @@ use IEEE.MATH_REAL.ALL;
 
 entity mono_moving_average is
     Generic(
-		DATA_LENGTH	:	Integer:= 24;
-		CLK_FREQUENCY : Integer :=100_000_000;
-        n_samples : integer := 32
+		DATA_LENGTH		: Integer := 24;
+		CLK_FREQUENCY 	: Integer := 100_000_000;
+        n_samples 		: integer := 32
     );
     Port (
-        aclk     : in std_logic;
-        aresetn     : in std_logic;
+        aclk     		: in std_logic;
+        aresetn     	: in std_logic;
         
-        filter_enable : in std_logic;
+        filter_enable 	: in std_logic;
         
-        din : in std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
-        dout : out std_logic_vector(DATA_LENGTH-1 DOWNTO 0)
+        din 			: in  std_logic_vector(DATA_LENGTH-1 DOWNTO 0);
+        dout 			: out std_logic_vector(DATA_LENGTH-1 DOWNTO 0)
     );
 end mono_moving_average;
 
 architecture Behavioral of mono_moving_average is
-
+	
+	-- CONSTANT DECLARATION --
+	-- extra bits needed fot the sum of the 32 samples  
 	constant extra_bits : integer := integer(log2(real(n_samples)));
+	
+	-- Audio sampling rate
 	constant Audio_Frequency : integer := 44_100;
+	
 	--Maximum Frequency rated in this block is 300MHz so 
 	constant Counter_Up_Value_un : unsigned(12 downto 0):= to_unsigned(CLK_FREQUENCY/Audio_Frequency,13);
     
+	
+	-- SIGNALS --
+	-- we need this signal to know when the filtering action is active 
     signal filter_check : std_logic := '0';
     
+	-- memory to store the 32 samples and compute the moving average 
     type filter_type is array(n_samples-1 DOWNTO 0) of signed(DATA_LENGTH-1 DOWNTO 0);
     signal filter_mem : filter_type := (Others => (Others => '0'));
     
+	-- signal for the sum of the samples 
     signal sum : signed(DATA_LENGTH-1 + extra_bits DOWNTO 0) := (Others => '0');
 
+	-- Since the samples rate is lower than the clk frequency we need a counter that show us how many clk cycles ...
+	--	... there are before the new sample, to properly store the data in memory. 
     signal count : unsigned(12 downto 0) := (Others => '0') ;
 
 begin
@@ -44,23 +56,32 @@ begin
         if rising_edge(aclk) then
             
             if aresetn = '0' then
-                filter_mem <= (Others => (Others => '0'));
-                sum <= (Others => '0');
+                filter_mem   <= (Others => (Others => '0'));
+                sum          <= (Others => '0');
                 filter_check <= '0';
+				count        <= (Others => '0') ;
                 
             else
+			
+				-- when the filter_enable is high we change the status of filtering : if filter_check = '1' the filtering is active ...
+				-- ... when filter_check = '0' there is no filtering. 
                 if filter_enable = '1' then
                     filter_check <= not filter_check;
                 end if;
 				
 				count <= count + 1 ;
 				
+				-- When we reach the value CLK_FREQUENCY/Audio_Frequency we use the new sample to update the sum and the memory 
                 if count = Counter_Up_Value_un then 
+				
                     -- We add the new value to the current total sum while at the same time we get rid of the oldest value
-                    sum <= sum - signed(filter_mem(filter_mem'HIGH)) + signed(din);
+                    sum        <= sum - signed(filter_mem(filter_mem'HIGH)) + signed(din);
+					
                     -- We perform the replacement in the memory as well
                     filter_mem <= filter_mem(filter_mem'HIGH-1 DOWNTO 0) & signed(din);
-                    count <= (Others => '0') ;
+					
+					-- Since we collect the new sample we reset the count to zero 
+                    count      <= (Others => '0') ;
                 end if ;
 				
                 if filter_check = '1' then
