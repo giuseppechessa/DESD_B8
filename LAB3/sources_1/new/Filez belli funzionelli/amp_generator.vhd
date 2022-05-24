4d6f6791889fc45b25e256afa5acd2d10bf91d48
TeoVallone <102909361+TeoVallone@users.noreply.github.com>
2022-05-24 22:30:31 +0200

    commenta

--- LAB3/sources_1/new/Filez belli funzionelli/amp_generator.vhd
+++ LAB3/sources_1/new/Filez belli funzionelli/amp_generator.vhd
@@ -17,7 +17,7 @@ entity amp_generator is
         
         volume : in std_logic_vector(9 DOWNTO 0);
 
-        -- After setting the upper and lower limits for jstk_units, the highest amp_power we can achieve is 16 for jstk_units = 5.
+        -- After setting the upper and lower limits for jstk_units, the highest amp_power we can achieve is 16 for jstk_units = 5, so 5 bits are enough.
         amp_power : out std_logic_vector(9-jstk_units DOWNTO 0);
         amp_sign : out std_logic
     );
@@ -46,20 +46,29 @@ begin
                 
             else
                 
+                -- We center the value of the volume at 0 between -512 and +511.
                 volume_sym_int <= to_integer(unsigned(volume)) - CENTER_VALUE;
                 
                 volume_sym <= to_signed(volume_sym_int, 10);
                 
                 amp_check(1 DOWNTO 0) <= volume_sym(jstk_units DOWNTO jstk_units-1);
                 
+                -- We shift right the obtained value n=jstk_units times, which gives us a value between -16 and 16 for n=5, between..
+                -- .. -8 and +8 for n=6, between -4 and +4 for n=7 and so on. 
                 volume_div <= shift_right(volume_sym, jstk_units);
                 
+                -- We noticed through simulations that the value we obtained from volume_div wasn't always the exact amplification, in particular we..
+                -- .. realized that, depending on the value of jstk_units, the first or second half of each "interval" was wrong by 1: for example, ..
+                -- .. with jstk_units=6 we were getting an amplification power of 0 (corresponding to 2**0=1) between +32 and +63 instead of 1, between..
+                -- .. +96 and +127 we were getting 1 instead of 2 and so on. We were able to fix this by checking the bits we stored in amp_check, ..
+                -- .. corresponding to the least significant bit after the shift_right along with the most significant bit that was erased in the division.
                 if amp_check = "00" or amp_check = "10" then
                     amp_sig <= to_integer(volume_div);
                 elsif amp_check = "01" or amp_check = "11" then
                     amp_sig <= to_integer(volume_div) + 1;
                 end if;
                 
+                -- Once we have the actual power of the amplification, we send its absolute value to the next set of modules along with the sign of the power.
                 if amp_sig < 0 then
                     amp_power <= std_logic_vector(to_signed(-amp_sig, amp_power'LENGTH));
                     amp_sign <= '1';
