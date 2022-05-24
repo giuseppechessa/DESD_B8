7144d15a9aa4cd50e693a53963f92692a0d3733b
GppChss <giuseppe.chessa@mail.polimi.it>
2022-05-24 15:50:40 +0200

    mono_moving_average added generic

--- LAB3/sources_1/new/Filez belli funzionelli/mono_moving_average.vhd
+++ LAB3/sources_1/new/Filez belli funzionelli/mono_moving_average.vhd
@@ -6,6 +6,7 @@ use IEEE.MATH_REAL.ALL;
 entity mono_moving_average is
     Generic(
 		DATA_LENGTH	:	Integer:= 24;
+		CLK_FREQUENCY : Integer :=100_000_000;
         n_samples : integer := 32
     );
     Port (
@@ -20,17 +21,20 @@ entity mono_moving_average is
 end mono_moving_average;
 
 architecture Behavioral of mono_moving_average is
+
+	constant extra_bits : integer := integer(log2(real(n_samples)));
+	constant Audio_Frequency : integer := 44_100;
+	--Maximum Frequency rated in this block is 300MHz so 
+	constant Counter_Up_Value_un : unsigned(12 downto 0):= to_unsigned(CLK_FREQUENCY/Audio_Frequency,13);
     
     signal filter_check : std_logic := '0';
-    
-    constant extra_bits : integer := integer(log2(real(n_samples)));
     
     type filter_type is array(n_samples-1 DOWNTO 0) of signed(DATA_LENGTH-1 DOWNTO 0);
     signal filter_mem : filter_type := (Others => (Others => '0'));
     
-    signal sum : signed(DATA_LENGTH-1 + extra_bits + 1 DOWNTO 0) := (Others => '0');
+    signal sum : signed(DATA_LENGTH-1 + extra_bits DOWNTO 0) := (Others => '0');
 
-    signal count : unsigned(11 downto 0) := (Others => '0') ;
+    signal count : unsigned(12 downto 0) := (Others => '0') ;
 
 begin
 
@@ -51,7 +55,7 @@ begin
 				
 				count <= count + 1 ;
 				
-                if count = 2270 then 
+                if count = Counter_Up_Value_un then 
                     -- We add the new value to the current total sum while at the same time we get rid of the oldest value
                     sum <= sum - signed(filter_mem(filter_mem'HIGH)) + signed(din);
                     -- We perform the replacement in the memory as well
@@ -61,7 +65,7 @@ begin
 				
                 if filter_check = '1' then
                     -- The output is made by the sum of the n_samples current values in the memory, shifted rightwards by extra_bits
-                    dout <= std_logic_vector(sum(sum'HIGH-1 DOWNTO extra_bits)); 
+                    dout <= std_logic_vector(sum(sum'HIGH DOWNTO extra_bits)); 
                 else
                     -- If we're not enabling the computation, we may send the inout data to the output as it is.
                     dout <= din;
